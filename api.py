10ecb3b29d359e661da7f7c8ac4e29ad72865e76
Petr Gabrlik <petrgabrlik@email.cz>
2017-03-18 17:19:26 +0100

    redis added

--- api.py
+++ api.py
@@ -5,11 +5,19 @@ API for pep8 validator
 
 from flask import Flask, render_template, request
 import json
-import redis
+from redis import StrictRedis
 import app as myapp
 
 
 app = Flask(__name__)
+
+
+redis_config = {
+    'host': '146.185.172.28',
+    'password': 'asorGYGAhJ1ybSCrWc2l5h8mKYk',
+    'port': 6379
+}
+redis = StrictRedis(**redis_config)
 
 
 @app.route('/')
@@ -21,15 +29,31 @@ def index():
 def check():
     if request.method == 'POST':
         code = request.form.get('code', '')
+        # print(myapp.check_pep8(code))
         results = myapp.check_pep8(code)
 
-        json_out = json.dumps(results)
-        # json_out = request.form.get('json_output', False)
-        print(json_out)
+        # REDIS
+        # redis_data = redis.get(code)
+        # print(redis_data)
+        # if redis_data:
+        #     # from redis
+        #     print('from redis')
+        #     results = json.loads(redis_data.decode('utf-8'))
+        # else:
+        #     # from app
+        #     print('from app')
+        #     results = myapp.check_pep8(code)
+        #     redis.set(code, json.dumps(results))
+
+        # JSON OUT
+        # results = json.dumps(results)
+
+        print(results)
 
         return render_template('index.html', title='Results', results=results, code=code)
-    else:
-        return render_template('index.html', title='Results', results=results, code=code)
+
+    elif request.method == 'GET':
+        return render_template('index.html', title='Home')
 
 
 if __name__ == '__main__':
