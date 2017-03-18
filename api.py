422d7d558afac568516808830141fbb503672de8
Petr Gabrlik <petrgabrlik@email.cz>
2017-03-18 16:34:08 +0100

    user output works

--- api.py
+++ api.py
@@ -3,8 +3,9 @@ API for pep8 validator
 '''
 
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+import json
+import redis
 import app as myapp
 
 
@@ -12,15 +13,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html', title='Home')
 
 
-# @app.route('/check_pep8', methods='POST')
-# def check():
-#     pass
+@app.route('/check_pep8', methods=['POST', 'GET'])
+def check():
+    if request.method == 'POST':
+        code = request.form.get('code', '')
+        results = myapp.check_pep8(code)
 
+        json_out = json.dumps(results)
+        # json_out = request.form.get('json_output', False)
+        print(json_out)
+
+        return render_template('index.html', title='Results', results=results, code=code)
+    else:
+        return render_template('index.html', title='Results', results=results, code=code)
 
 
 if __name__ == '__main__':
